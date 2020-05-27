aae7b08d1a6f190bcd5e2aafea36c8923caf4a88
JP <paulotteodosio@gmail.com>
2020-05-27 18:12:47 -0300

    Add Transaction History

--- src/pages/send/Send.js
+++ src/pages/send/Send.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import style from "./Send.module.css";
 import Header from "./components/header/Header";
 import SendForm from "./components/sendForm/SendForm";
-import {getBalance, getAddress, createNewWalletKeyPair} from "../../lib/sapi";
+import { getBalance, getAddress, createNewWalletKeyPair } from "../../lib/sapi";
 import { isAddress, isPK } from "../../lib/smart";
 import { useForm } from "react-hook-form";
 import useModal from "../../util/useModal";
@@ -10,12 +10,14 @@ import Modal from "./components/modal/Modal";
 import barcode from "../../assets/images/barcode.svg";
 import { NavLink } from "react-router-dom";
 import generatePDF from "../export-private-keys/GeneratorPDF";
+import TransactionsHistory from "./components/transactions-history/TransactionHistory";
 
 function Send() {
   const { isShowing, toggle } = useModal(false);
   const [address, setAddress] = useState();
   const [privateKey, setPrivateKey] = useState();
   const [balance, setBalance] = useState(false);
+  const [showTransactions, setShowTransactions] = useState(false);
   const {
     register,
     errors,
@@ -38,12 +40,12 @@ function Send() {
     let isValid = false;
 
     await isAddress(value)
-      .then(data => {
+      .then((data) => {
         setAddress(data);
         getBalanceFromSAPI(data);
         isValid = true;
       })
-      .catch(data => data);
+      .catch((data) => data);
 
     await isPK(value)
       .then(() => {
@@ -53,81 +55,95 @@ function Send() {
         getBalanceFromSAPI(address);
         isValid = true;
       })
-      .catch(data => data);
+      .catch((data) => data);
 
     if (!isValid) {
       setError("address", "invalid", "Invalid Address");
     }
 
     return isValid;
-  }
+  };
 
   return (
     <div className={style.root}>
+      <Header />
 
-        <Header />
-
-        <div className="container">
-          <div className="cardWrapper">
-            <div className="formControl">
-              <label>
-                Your Address or Private Key
-                <input
-                  type="text"
-                  name="address"
-                  autoComplete="off"
-                  ref={register({
-                    required: true,
-                    validate: AddressPKValidation,
-                  })}
-                  onInput={() => triggerValidation("addressTo")}
-                />
-              </label>
-              <button type="button" className="modalButton" onClick={toggle}>
-                <img className="barCode" src={barcode} alt="Barcode" />
-              </button>
-              <Modal
-                isShowing={isShowing}
-                hide={toggle}
-                callback={(obj) =>
-                  obj.address && setValue("address", obj.address, true)
-                }
+      <div className="container">
+        <div className="cardWrapper">
+          <div className="formControl">
+            <label>
+              Your Address or Private Key
+              <input
+                type="text"
+                name="address"
+                autoComplete="off"
+                ref={register({
+                  required: true,
+                  validate: AddressPKValidation,
+                })}
+                onInput={() => triggerValidation("addressTo")}
               />
-              {errors.address && (
-                <span className="error-message">{errors.address.message}</span>
-              )}
+            </label>
+            <button type="button" className="modalButton" onClick={toggle}>
+              <img className="barCode" src={barcode} alt="Barcode" />
+            </button>
+            <Modal
+              isShowing={isShowing}
+              hide={toggle}
+              callback={(obj) =>
+                obj.address && setValue("address", obj.address, true)
+              }
+            />
+            {errors.address && (
+              <span className="error-message">{errors.address.message}</span>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {formState.isValid ? (
+        <div>
+          <div className="container">
+            <div className={style.btnWrapper}>
+              <p>Your Balance: {balance}</p>
+              <button
+                className={style.btn}
+                onClick={(e) => setShowTransactions(!showTransactions)}
+              >
+                Show Last Transactions
+              </button>
+            </div>
+          </div>
+
+          <TransactionsHistory
+            address={address}
+            isShowing={showTransactions}
+            hide={() => setShowTransactions(false)}
+          />
+
+          <div className="container">
+            <div className="cardWrapper">
+              <SendForm
+                address={address}
+                balance={balance}
+                privateKey={privateKey}
+              />
             </div>
           </div>
         </div>
-
-        {formState.isValid ? (
-          <div>
-            <div className="container">
-              <div className={style.btnWrapper}>
-                <p>Your Balance: {balance}</p>
-                <a
-                  className={style.btn}
-                  href={`https://insight.smartcash.cc/address/${address}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Show Transactions
-                </a>
-              </div>
-            </div>
-
-            <div className="container">
-              <div className="cardWrapper">
-                <SendForm address={address} balance={balance} privateKey={privateKey}/>
-              </div>
-            </div>
-          </div>
-        ) : null}
+      ) : null}
 
       {!formState.isValid ? (
         <>
           <div className="container">
-            <button className={`btn ${style.newAddress}`} onClick={() => generatePDF([createNewWalletKeyPair()], 'SmartCash_Address')}>Generate paper wallet</button>
+            <button
+              className={`btn ${style.newAddress}`}
+              onClick={() =>
+                generatePDF([createNewWalletKeyPair()], "SmartCash_Address")
+              }
+            >
+              Generate paper wallet
+            </button>
           </div>
           <div className="container">
             <div className={style.orSpan}>
@@ -141,8 +157,6 @@ function Send() {
           </div>
         </>
       ) : null}
-
-
     </div>
   );
 }
