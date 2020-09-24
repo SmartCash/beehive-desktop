ca0bb4e3c27ac88d4d9d83b703945c41f5561d74
Enrique Soares <enrique.soares@finstro.com.au>
2020-09-24 03:59:51 -0300

    decentralized SAPIs

--- src/components/RedirectToHttps.js
+++ src/components/RedirectToHttps.js
@@ -3,7 +3,7 @@ const isLocalHost = (hostname) =>
         hostname === 'localhost' ||
         hostname === '[::1]' ||
         hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/) ||
-        window.location.port === 8080
+        window.location.port === '8080'
     );
 
 const HttpsRedirect = ({ disabled, children }) => {
