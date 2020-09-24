50799077ef288cfcd53531c3bbaf98aefd517d80
Enrique Soares <enrique.soares@finstro.com.au>
2020-09-24 03:40:12 -0300

    decentralized SAPIs

--- src/lib/poolSapi.js
+++ src/lib/poolSapi.js
@@ -1,9 +1,35 @@
 const random = require('random');
 const request = require('request-promise');
 
-let sapis = ['https://sapi2.smartcash.org', 'https://sapi.smartcash.cc'];
+let sapis = [
+    `http://64.44.52.40:8080`,
+    `http://167.86.103.167:8080`,
+    `http://90.145.247.78:8080`,
+    `http://90.145.247.54:8080`,
+    `http://62.171.171.66:8080`,
+    `http://195.201.12.93:8080`,
+    `http://167.86.79.4:8080`,
+    `http://135.181.85.180:8080`,
+    `http://135.181.85.177:8080`,
+    `http://135.181.85.186:8080`,
+    `http://135.181.85.188:8080`,
+    `http://135.181.85.176:8080`,
+    `http://135.181.85.183:8080`,
+    `http://135.181.85.184:8080`,
+    `http://135.181.85.187:8080`,
+    `http://135.181.85.182:8080`,
+    `http://135.181.85.185:8080`,
+    `http://195.201.22.100:8080`,
+    `http://195.201.22.122:8080`,
+    `http://195.201.22.110:8080`,
+    `http://195.201.22.103:8080`,
+    `http://195.201.22.112:8080`,
+    `http://195.201.22.113:8080`,
+    `http://195.201.22.117:8080`,
+    `http://195.201.22.101:8080`,
+];
 /*
-request.get('https://sapi.smartcash.cc/v1/smartnodes/list').then((list) => {
+request.get('https://sapi.smartcash.ccsmartnodes/list').then((list) => {
     list = JSON.parse(list);
 
     const mappedKey = Object.keys(list).map((key) => {
