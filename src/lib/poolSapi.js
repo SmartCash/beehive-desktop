7b5ad428ea79f7177dec4ca81402df724baee680
Enrique Souza <enrique.soares@finstro.com.au>
2020-09-24 21:03:09 -0300

    http issue

--- src/lib/poolSapi.js
+++ src/lib/poolSapi.js
@@ -33,28 +33,4 @@ if (window.location.protocol === 'https:') {
     sapis = sapis[`https://sapi.smartcash.cc`];
 }
 
-/*
-request.get('https://sapi.smartcash.ccsmartnodes/list').then((list) => {
-    list = JSON.parse(list);
-
-    const mappedKey = Object.keys(list).map((key) => {
-        return list[key];
-    });
-
-    const serves = mappedKey
-        .filter((f) => f.protocol === 90030 && f.status === 'ENABLED')
-        .map((server) => {
-            return {
-                status: server.status,
-                protocol: server.protocol,
-                payee: server.payee,
-                uptime: server.uptime,
-                ip_sapi: server.ip.split(':')[0] + ':8080',
-                ip: server.ip,
-            };
-        });
-    if (serves.length > 0) {
-        sapis = serves.map((server) => 'http://' + server.ip_sapi);
-    }
-});*/
 module.exports = () => sapis[random.int(0, sapis.length - 1)];
