225430bf63ba44db804f8dd95138ecea6cb12243
JP <jp@kodelabs.com.br>
2020-10-25 16:56:41 -0300

    refactor modify transactions page

--- src/pages/transactions/Transactions.js
+++ src/pages/transactions/Transactions.js
@@ -8,39 +8,47 @@ import { Scrollbars } from 'react-custom-scrollbars';
 function Transactions() {
     const { walletCurrent } = useContext(WalletContext);
     const [history, setHistory] = useState([]);
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState();
 
     useEffect(() => {
         async function _getTransactionHistory() {
+            setLoading(true);
+            setError(null);
+            setHistory([]);
             await getTransactionHistory(walletCurrent)
                 .then((data) => setHistory(data))
-                .catch((error) => console.error(error));
+                .catch(() => setError('There is no transactions for this wallet'))
+                .finally(() => setLoading(false));
         }
         _getTransactionHistory();
     }, [walletCurrent]);
 
     return (
-        <Page className='page-transactions'>
-            <Scrollbars>
-                {history?.map((tx, index) => {
-                    return (
-                        <div className="transaction" key={index}>
-                            <p className="label">Type</p>
-                            <p className="value">{tx.direction}</p>
-                            <p className="label">Fees</p>
-                            <p className="value">{tx.fees}</p>
-                            <p className="label">Amount</p>
-                            <p className="value">{tx.amount}</p>
-                            <p className="label">Transaction Id</p>
-                            <p className="value">
-                                <a href={`https://explorer.smartcash.org/#/tx/${tx.txid}`} target="_blank">
-                                    {tx.txid}
-                                </a>
-                            </p>
-                        </div>
-                    );
-                })}
-            </Scrollbars>
+        <Page className="page-transactions">
+            {loading && <p className="error">Loading Transactions</p>}
+            {error && <p className="error">{error}</p>}
+            {!error && history && (
+                <Scrollbars>
+                    {history?.map((tx, index) => {
+                        return (
+                            <div className="transaction" key={index}>
+                                <p className="label">Type</p>
+                                <p className="value">{tx.direction}</p>
+                                <p className="label">Amount</p>
+                                <p className="value">{tx.amount}</p>
+                                <p className="label">Transaction Id</p>
+                                <p className="value">
+                                    <a href={`https://explorer.smartcash.org/#/tx/${tx.txid}`} target="_blank">
+                                        {tx.txid}
+                                    </a>
+                                </p>
+                            </div>
+                        );
+                    })}
+                </Scrollbars>
+            )}
         </Page>
-    )
+    );
 }
 export default Transactions;
