a244ef1dca9e76cccf9505961b8b620e99ce48da
JP <jp@kodelabs.com.br>
2020-11-30 23:59:33 -0300

    Add react-error-overlay package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './index.css';
+import { stopReportingRuntimeErrors } from "react-error-overlay";
+
+if (process.env.NODE_ENV === "development") {
+  stopReportingRuntimeErrors();
+}
 
 ReactDOM.render(
     <React.StrictMode>
