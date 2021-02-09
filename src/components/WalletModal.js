ecc0ca518a9bbf773b85a8130de21f539b665030
JP <jp@kodelabs.com.br>
2021-02-08 23:27:50 -0300

    Fix WalletModal.js

--- src/components/WalletModal.js
+++ src/components/WalletModal.js
@@ -10,36 +10,47 @@ import * as CryptoJS from 'crypto-js';
 function WalletModal({ isShowing, hide, disableCloseButton }) {
     const [wallet, setWallet] = useState();
     const [createWallet, setCreateWallet] = useState(false);
-    const { addWallet } = useContext(WalletContext);
+    const { addWallet, decryptWallets } = useContext(WalletContext);
     const [privateKey, setPrivateKey] = useState();
-    const [isPKInvalid, setIsPKInvalid] = useState(false);
+    const [error, setError] = useState('');
     const [_password, setPassword] = useState();
 
-    const handleAddWallet = () => {
-        generatePDF([wallet], 'SmartCash_Address');
-        const _wallet = {
-            privateKey: CryptoJS.AES.encrypt(wallet.privateKey, _password).toString(),
-            address: wallet.address,
-        };
-        addWallet(_wallet, _password);
-        setCreateWallet(false);
-        hide();
+    const handleAddWallet = async () => {
+        const wallets = await decryptWallets(_password);
+        if (wallets && wallets.length > 0 || disableCloseButton) {
+            generatePDF([wallet], 'SmartCash_Address');
+            const _wallet = {
+                privateKey: CryptoJS.AES.encrypt(wallet.privateKey, _password).toString(),
+                address: wallet.address,
+            };
+            addWallet(_wallet, _password);
+            setCreateWallet(false);
+            hide();
+        } else {
+            setError('Do not possible decrypt local data using this password.');
+        }
     };
 
-    const handleImportPrivateKey = () => {
-        isPK(privateKey)
-            .then(() => {
-                const _wallet = {
-                    privateKey: CryptoJS.AES.encrypt(privateKey, _password).toString(),
-                    address: getAddress(privateKey),
-                };
-                addWallet(_wallet, _password);
-                hide();
-            })
-            .catch(() => setIsPKInvalid(true));
+    const handleImportPrivateKey = async () => {
+        const wallets = await decryptWallets(_password);
+        if (wallets && wallets.length > 0 || disableCloseButton) {
+            isPK(privateKey)
+                .then(() => {
+                    const _wallet = {
+                        privateKey: CryptoJS.AES.encrypt(privateKey, _password).toString(),
+                        address: getAddress(privateKey),
+                    };
+                    addWallet(_wallet, _password);
+                    hide();
+                })
+                .catch(() => setError('Invalid Private Key'));
+        } else {
+            setError('Do not possible decrypt local data using this password.');
+        }
     };
 
     const handleCreateNewOne = () => {
+        setError('');
         setCreateWallet(true);
         setWallet(createNewWalletKeyPair());
     };
@@ -47,6 +58,7 @@ function WalletModal({ isShowing, hide, disableCloseButton }) {
     const insertPrivateKey = (event) => setPrivateKey(event.target.value);
 
     const handleCloseModal = () => {
+        setError('');
         setCreateWallet(false);
         setWallet(null);
         hide();
@@ -76,7 +88,6 @@ function WalletModal({ isShowing, hide, disableCloseButton }) {
 
                         <div className={style['modal-body']}>
                             <div className={style['address-content']}>
-                                
                                 {!createWallet && (
                                     <div className={style['import-address']}>
                                         <h2>Import from Private Key</h2>
@@ -85,8 +96,12 @@ function WalletModal({ isShowing, hide, disableCloseButton }) {
                                             placeholder="Insert your private key here"
                                             rows={5}
                                         />
-                                        <input type="password" placeholder="Password" onInput={(event) => setPassword(event.target.value)}/>
-                                        {isPKInvalid && <p>Invalid Private Key</p>}
+                                        <input
+                                            type="password"
+                                            placeholder="Password"
+                                            onInput={(event) => setPassword(event.target.value)}
+                                        />
+                                        {error && <p>{error}</p>}
                                         <button onClick={handleImportPrivateKey}>Import</button>
                                         <button onClick={handleCreateNewOne}>Create new one</button>
                                     </div>
@@ -102,8 +117,13 @@ function WalletModal({ isShowing, hide, disableCloseButton }) {
                                             <p>{wallet.address}</p>
                                         </div>
                                         <div>
-                                            <input type="password" placeholder="Password" onInput={(event) => setPassword(event.target.value)}/>
+                                            <input
+                                                type="password"
+                                                placeholder="Password"
+                                                onInput={(event) => setPassword(event.target.value)}
+                                            />
                                         </div>
+                                        {error && <p>{error}</p>}
                                         <button className="btn" onClick={handleAddWallet}>
                                             Use this one and save as PDF
                                         </button>
