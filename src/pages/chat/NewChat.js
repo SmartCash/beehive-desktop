6063bb8f6b724815cac4059b9424e53da8070ffe
JP <paulotteodosio@gmail.com>
2021-02-12 02:10:51 -0300

    Add html structure in NewChat.js

--- src/pages/chat/NewChat.js
+++ src/pages/chat/NewChat.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './NewChat.css';
 
 export function NewChat() {
     return (
-        <React.Fragment>
-            <input placeholder="Address to talk" />
-            <button>Request solicitation</button>
-        </React.Fragment>
+        <div className="page-new-chat">
+            <div className="page-content">
+                <p className="title">New chat</p>
+                <p className="subtitle">Enter the wallet address to start a new chat</p>
+                <div className="address-form">
+                    <input autoFocus />
+                    <button>Request</button>
+                </div>
+            </div>
+        </div>
     )
 }
