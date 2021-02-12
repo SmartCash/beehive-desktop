735dfa8c65d8352d2a712b79bb2aea7c9b009258
JP <paulotteodosio@gmail.com>
2021-02-12 02:11:16 -0300

    modify prop tx to chat in ChatMessages.js

--- src/pages/chat/ChatMessages.js
+++ src/pages/chat/ChatMessages.js
@@ -2,30 +2,28 @@ import React from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 export function ChatMessages(props) {
-    const { tx } = props;
+    const { chat } = props;
     return (
         <div className="chat-messages">
             <div className="transaction chatAddress">
                 <p className="label">Chat Address</p>
-                <p className="value">{tx?.chatAddress}</p>
+                <p className="value">{chat?.chatAddress}</p>
             </div>
             <Scrollbars>
-                    {tx &&
-                        tx.messages
-                            .sort((a, b) => (a.time > b.time ? 1 : -1))
-                            .map((m) => {
-                                return (
-                                    <div className={`transaction message message-${m.direction}`} key={m.time}>
-                                        <p className="value">{m.message}</p>
-                                        <p className="label">{new Date(m.time * 1000).toLocaleString()}</p>
-                                    </div>
-                                );
-                            })}
-                </Scrollbars>
+                {chat?.messages
+                    .map((m) => {
+                        return (
+                            <div className={`transaction message message-${m.direction}`} key={m.time}>
+                                <p className="value">{m.message}</p>
+                                <p className="label">{new Date(m.time * 1000).toLocaleString()}</p>
+                            </div>
+                        );
+                    })}
+            </Scrollbars>
             <div className="send-wrapper">
                 <textarea placeholder="Type a message..." className="send-input" />
                 <button className="btn send-button">Send</button>
             </div>
         </div>
-    )
+    );
 }
