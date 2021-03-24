dd15e881854ff0b85b174d38c7fa816dafec37e0
Enrique Souza <enriquesouza@live.com>
2021-03-24 00:40:20 -0300

    smartcashjs-lib + transactions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { generatePhrase, getFromDerivationPaths, validatePhrase } = require('./src/lib/smart-mnemonic');
+const { mnemonic } = require('smartcashjs-lib/src/index');
 const TEST_PHRASE = 'grape front option already anxiety mixed public bulb final expose chef traffic';
 
-// console.log(generatePhrase());
-// console.log(validatePhrase({ words: TEST_PHRASE }));
-console.log(JSON.stringify(getFromDerivationPaths({ words: TEST_PHRASE })));
+// console.log(mnemonic.generatePhrase());
+// console.log(mnemonic.validatePhrase({ words: TEST_PHRASE }));
+console.log(JSON.stringify(mnemonic.getFromDerivationPaths({ words: TEST_PHRASE })));
