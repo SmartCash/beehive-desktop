304d7464bd763dfd570451e9ff8eb2165ab2c24a
JP <jp@kodelabs.com.br>
2021-03-31 12:30:27 -0300

    remove package react-error-overlay

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Startup } from 'application/startup';
-import { stopReportingRuntimeErrors } from 'react-error-overlay';
 
 // Styles
 // import 'presentation/styles/index.scss';
 import 'presentation/styles/index.css';
-
-if (process.env.NODE_ENV === 'development') {
-    stopReportingRuntimeErrors();
-}
 
 ReactDOM.render(
     <React.StrictMode>
