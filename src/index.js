e016e862c04a55e3d56fdd555c4fc96a76b8819e
JP <jp@kodelabs.com.br>
2021-03-31 12:26:47 -0300

    add bootstrap and animate.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Startup } from 'application/startup';
 import { stopReportingRuntimeErrors } from 'react-error-overlay';
 
 // Styles
+// import 'presentation/styles/index.scss';
 import 'presentation/styles/index.css';
 
 if (process.env.NODE_ENV === 'development') {
