771ff473bf8b9a65e505bb441995d65ffbd2e560
Enrique Souza <enriquesouza@live.com>
2021-04-09 00:01:59 -0600

    git dependency per commit hash

--- src/presentation/components/button-download-wallets/button-download-wallets.js
+++ src/presentation/components/button-download-wallets/button-download-wallets.js
@@ -3,18 +3,18 @@ import React, { useContext } from 'react';
 import { ReactComponent as IconDownload } from 'presentation/assets/images/download.svg';
 import useModal from 'application/hooks/useModal';
 import { PasswordModal } from '../password-modal/passsword-modal';
-import generatePDF from 'application/lib/GeneratorPDF';
-import { tryToDecryptAES } from 'application/lib/sapi';
+
+import { generatePDF } from 'smartcashjs-lib/src/sapi/GeneratorPDF';
 
 export function ButtonDownloadWallets() {
     const { wallets } = useContext(WalletContext);
     const { isShowing, toggle } = useModal();
 
     function downloadWallets(password) {
-        const walletsShadowClone = JSON.parse(JSON.stringify(wallets))
+        const walletsShadowClone = JSON.parse(JSON.stringify(wallets));
 
         for (let wallet of walletsShadowClone) {
-            wallet.privateKey = tryToDecryptAES({ text: wallet.privateKey, password })
+            wallet.privateKey = tryToDecryptAES({ text: wallet.privateKey, password });
         }
 
         generatePDF({ wallets: walletsShadowClone, filename: `MyWallets_SmartCash_${Date.now()}` });
