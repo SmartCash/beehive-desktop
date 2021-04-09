04b5718be9ae95981501427cc3958ea8cbba23dd
Enrique Souza <enriquesouza@live.com>
2021-04-08 18:04:42 -0600

    remove unecessary files

--- src/application/lib/encryption.js
+++
@@ -1,9 +0,0 @@
-import * as CryptoJS from 'crypto-js';
-
-export function encrypt(dataToEncrypt, secret) {
-    return CryptoJS.AES.encrypt(dataToEncrypt, secret).toString();
-}
-
-export function decrypt(encryptedData, secret) {
-    return CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(encryptedData, secret));
-}
