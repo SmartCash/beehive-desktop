23881a89b2671ca347aa8aa2e6c770e0b489db73
Enrique Souza <enriquesouza@live.com>
2021-04-19 00:37:08 -0400

    hid message on send screen

--- src/presentation/pages/send/Send.js
+++ src/presentation/pages/send/Send.js
@@ -38,7 +38,7 @@ function SendComponent() {
         messageToSend,
         setMessageToSend,
         TXIDError,
-        TXIDLoading
+        TXIDLoading,
     } = useContext(SendContext);
 
     const debouncedAmount = useDebounce(amountToSend, 1500);
@@ -46,7 +46,7 @@ function SendComponent() {
     const { password, setPassword, hideBalance } = useContext(WalletContext);
 
     useEffect(() => {
-        if (debouncedAmount) {            
+        if (debouncedAmount) {
             checkAmounToSendError(debouncedAmount);
         }
     }, [debouncedAmount]);
@@ -66,12 +66,12 @@ function SendComponent() {
                         </button>
                     </p>
                     <p>
-                        Balance 
-                        <Balance value={ walletCurrentBalance.unlocked } />                    
+                        Balance
+                        <Balance value={walletCurrentBalance.unlocked} />
                     </p>
                     <p>
-                        Locked 
-                        <Balance value={ walletCurrentBalance.locked } />
+                        Locked
+                        <Balance value={walletCurrentBalance.locked} />
                     </p>
                 </div>
                 <div className="walletEmpty">
@@ -81,27 +81,26 @@ function SendComponent() {
         );
     }
 
-    function getPass(){
+    function getPass() {
         return password;
     }
 
-    function send(){
+    function send() {
         var pass = getPass();
 
-        if(!pass){
+        if (!pass) {
             togglePasswordModal();
-        } else{
-            submitSendAmount(pass)
-        }        
+        } else {
+            submitSendAmount(pass);
+        }
     }
 
     const handleSend = (pass, saveInContext) => {
-        if(saveInContext)
-            setPassword(pass);             
-        
-        submitSendAmount(pass)
+        if (saveInContext) setPassword(pass);
+
+        submitSendAmount(pass);
         togglePasswordModal();
-    }  
+    };
 
     return (
         <Page className="page-send">
@@ -111,147 +110,169 @@ function SendComponent() {
                         <img src={loader} alt={'loading...'} />
                     </p>
                 )}
-                
+
                 {!TXIDLoading && (
                     <>
-                        
-                {TXIDError && <p className="SendError">{TXIDError}</p>}
-               
+                        {TXIDError && <p className="SendError">{TXIDError}</p>}
 
-                {TXID && (
-                    <div className="hasBeenSent">
-                        <button className="btnClose" onClick={() => clearTxId()}>X</button>
-                        <p><strong>Amount has been sent</strong></p>
-                        <div className="msgSuccess">Transaction ID: </div>
-                        <strong className="txID">{TXID}</strong>
+                        {TXID && (
+                            <div className="hasBeenSent">
+                                <button className="btnClose" onClick={() => clearTxId()}>
+                                    X
+                                </button>
+                                <p>
+                                    <strong>Amount has been sent</strong>
+                                </p>
+                                <div className="msgSuccess">Transaction ID: </div>
+                                <strong className="txID">{TXID}</strong>
 
-                        <div>
-                            <ul className="links">
-                                <li><button className="link" title="Copy address to clipboard" onClick={() => electron.clipboard.writeText(TXID)}>Copy</button></li>
-                                <li><button className="link" onClick={() => electron.shell.openExternal(`https://insight.smartcash.cc/tx/${TXID}`)}>Open into Insight</button></li>
-                                <li><button className="link" onClick={() => electron.shell.openExternal(`http://explorer.smartcash.cc/tx/${TXID}`)}>Open into Sapi Explorer</button></li>
-                            </ul>                                                                                                                    
-                        </div>                                    
-                    </div>
-                )}
+                                <div>
+                                    <ul className="links">
+                                        <li>
+                                            <button
+                                                className="link"
+                                                title="Copy address to clipboard"
+                                                onClick={() => electron.clipboard.writeText(TXID)}
+                                            >
+                                                Copy
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button
+                                                className="link"
+                                                onClick={() =>
+                                                    electron.shell.openExternal(`https://insight.smartcash.cc/tx/${TXID}`)
+                                                }
+                                            >
+                                                Open into Insight
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button
+                                                className="link"
+                                                onClick={() =>
+                                                    electron.shell.openExternal(`http://explorer.smartcash.cc/tx/${TXID}`)
+                                                }
+                                            >
+                                                Open into Sapi Explorer
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        )}
 
-                <div className="form-control privateKey">
-                    <p>
-                        Sending from <span>{walletCurrent} </span>
-                        <button
-                            className="btn copy"
-                            title="Copy address to clipboard"
-                            onClick={() => electron.clipboard.writeText(walletCurrent)}
-                        >
-                            <IconCopy className="btnCopy" />
+                        <div className="form-control privateKey">
+                            <p>
+                                Sending from <span>{walletCurrent} </span>
+                                <button
+                                    className="btn copy"
+                                    title="Copy address to clipboard"
+                                    onClick={() => electron.clipboard.writeText(walletCurrent)}
+                                >
+                                    <IconCopy className="btnCopy" />
+                                </button>
+                            </p>
+                            <p>
+                                Balance{' '}
+                                <span>
+                                    <Balance value={walletCurrentBalance?.unlocked} />
+                                </span>
+                            </p>
+                            <p>
+                                Locked{' '}
+                                <span>
+                                    <Balance value={walletCurrentBalance?.locked} />
+                                </span>
+                            </p>
+                        </div>
+                        <div className="form-group">
+                            <div className="form-control address">
+                                <label htmlFor="addressTo">Send funds to address</label>
+                                <input
+                                    id="addressTo"
+                                    placeholder="Insert address here"
+                                    autoComplete="off"
+                                    type="text"
+                                    value={addressToSend}
+                                    onInput={(event) => setAddressToSend(event.target.value.trim())}
+                                />
+                                {addressToSendError && <p className="invalidAddress">Invalid address</p>}
+                            </div>
+
+                            <div className="form-control amount">
+                                <label htmlFor="amount">Amount to send</label>
+                                <MaskedInput
+                                    mask={currencyMask}
+                                    id="amount"
+                                    value={amountToSend}
+                                    onInput={(event) => setAmountToSend(event.target.value)}
+                                    autoComplete="off"
+                                />
+                                {amountToSendError && <p className="amountToSendError">{amountToSendError}</p>}
+                            </div>
+
+                            {isSmartFiat() && (
+                                <div className="form-control amount">
+                                    <button type="button" onClick={() => calculateSendAll(1)}>
+                                        Send All
+                                    </button>
+                                </div>
+                            )}
+
+                            <div className="form-control message" style={{ display: 'none' }}>
+                                <label htmlFor="message">Write your message</label>
+                                <input
+                                    id="messageTo"
+                                    placeholder="Insert message here"
+                                    autoComplete="off"
+                                    type="text"
+                                    value={messageToSend}
+                                    onInput={(event) => {
+                                        setMessageToSend(event.target.value);
+                                        calculateSendAmount(event.target.value);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        {netFee && (
+                            <div className="transactionInfo">
+                                <div>
+                                    <p className="label">Net fee</p>
+                                    <p className="value">
+                                        {netFee
+                                            .toLocaleString('en-US', {
+                                                style: 'currency',
+                                                currency: 'USD',
+                                                minimumFractionDigits: 4,
+                                            })
+                                            .replace('$', '∑')}
+                                    </p>
+                                </div>
+                                <div>
+                                    <p className="label">Total in Smart</p>
+                                    <p className="value">
+                                        {totalInSmart
+                                            .toLocaleString('en-US', {
+                                                style: 'currency',
+                                                currency: 'USD',
+                                                minimumFractionDigits: 4,
+                                            })
+                                            .replace('$', '∑')}
+                                    </p>
+                                </div>
+                            </div>
+                        )}
+                        <button type="submit" onClick={() => send()} disabled={!canSend()}>
+                            Send
                         </button>
-                    </p>
-                    <p>
-                        Balance{' '}
-                        <span>
-                            <Balance value={ walletCurrentBalance?.unlocked } />
-                        </span>
-                    </p>
-                    <p>
-                        Locked{' '}
-                        <span>
-                            <Balance value={ walletCurrentBalance?.locked } />                       
-                        </span>
-                    </p>
-                </div>
-                <div className="form-group">
-                    <div className="form-control address">
-                        <label htmlFor="addressTo">Send funds to address</label>
-                        <input
-                            id="addressTo"
-                            placeholder="Insert address here"
-                            autoComplete="off"
-                            type="text"
-                            value={addressToSend}
-                            onInput={(event) => setAddressToSend(event.target.value.trim())}
-                        />
-                        {addressToSendError && <p className="invalidAddress">Invalid address</p>}
-                    </div>
-
-                    <div className="form-control amount">
-                        <label htmlFor="amount">Amount to send</label>
-                        <MaskedInput
-                            mask={currencyMask}
-                            id="amount"
-                            value={amountToSend}
-                            onInput={(event) => setAmountToSend(event.target.value)}
-                            autoComplete="off"
-                        />
-                        {amountToSendError && <p className="amountToSendError">{amountToSendError}</p>}
-                    </div>
-
-                    {isSmartFiat() && (
-                        <div className="form-control amount">
-                            <button type="button" onClick={() => calculateSendAll(1)}>
-                                Send All
-                            </button>
-                        </div>
-                    )}
-
-                    <div className="form-control message">
-                        <label htmlFor="message">Write your message</label>
-                        <input
-                            id="messageTo"
-                            placeholder="Insert message here"
-                            autoComplete="off"
-                            type="text"
-                            value={messageToSend}
-                            onInput={(event) => {
-                                setMessageToSend(event.target.value);
-                                calculateSendAmount(event.target.value);
-                            }}
-                        />
-                    </div>                    
-                </div>
-                {netFee && (
-                    <div className="transactionInfo">
-                        <div>
-                            <p className="label">Net fee</p>
-                            <p className="value">
-                                {netFee
-                                    .toLocaleString('en-US', {
-                                        style: 'currency',
-                                        currency: 'USD',
-                                        minimumFractionDigits: 4,
-                                    })
-                                    .replace('$', '∑')}
-                            </p>
-                        </div>
-                        <div>
-                            <p className="label">Total in Smart</p>
-                            <p className="value">
-                                {totalInSmart
-                                    .toLocaleString('en-US', {
-                                        style: 'currency',
-                                        currency: 'USD',
-                                        minimumFractionDigits: 4,
-                                    })
-                                    .replace('$', '∑')}
-                            </p>
-                        </div>
-                    </div>
+                    </>
                 )}
-                <button type="submit" onClick={() => send()} disabled={!canSend()}>
-                    Send
-                </button>           
-                </>
-                )}         
-                 </Scrollbars>
+            </Scrollbars>
 
-                {
-                    showPasswordModal && (
-                        <PasswordModal
-                            callBack={handleSend}
-                            isShowing={showPasswordModal}
-                            onClose={togglePasswordModal}
-                        />
-                    )
-                } 
+            {showPasswordModal && (
+                <PasswordModal callBack={handleSend} isShowing={showPasswordModal} onClose={togglePasswordModal} />
+            )}
         </Page>
     );
 }
